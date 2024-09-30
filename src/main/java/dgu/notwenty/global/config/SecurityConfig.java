4da9cb65f645e7adc5140d554fce1332b49d5576
haeun1107 <haeun9394@gmail.com>
2024-09-30 16:31:06 +0900

    Feat: swagger 세팅

--- src/main/java/dgu/notwenty/global/config/SecurityConfig.java
+++ src/main/java/dgu/notwenty/global/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package dgu.notwenty.global.config;public class SecurityConfig {
+package dgu.notwenty.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return httpSecurity.build();
+    }
 }
+
--- src/main/java/dgu/notwenty/global/config/SwaggerConfig.java
+++ src/main/java/dgu/notwenty/global/config/SwaggerConfig.java
@@ -1,2 +1,36 @@
-package dgu.notwenty.global.config;public class SwaggerConfig {
+package dgu.notwenty.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI Notwenty() {
+        io.swagger.v3.oas.models.info.Info info = new io.swagger.v3.oas.models.info.Info()
+                .title("Notwenty API")
+                .description("Notwenty API 명세서")
+                .version("1.0.0");
+
+        String jwtSchemeName = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("http://localhost:8080"))  // 절대 경로로 수정
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
 }
